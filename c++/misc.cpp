01f03fe10760bb6b14e72f7fe147256c3ab89654
work <work>
2020-09-10 08:33:10 +0200

    edit

--- c++/misc.cpp
+++ c++/misc.cpp
@@ -65,3 +65,32 @@ string readFile_v2(string filename) {
   return result;
 }
 
+
+string str_format(const char* fmt, ...) {
+  string result;
+  
+  va_list vl;
+  va_start(vl, fmt);
+  int size = _vscprintf(fmt, vl);
+  result.resize(++size);
+  vsnprintf_s((char*) result.data(), size, _TRUNCATE, fmt, vl);
+  va_end(vl);
+  
+  return result;
+}
+
+string str_format_v2(const char* fmt, ...) {
+  string result;
+
+  va_list args;
+  va_start(args, format);
+  size_t size = vsnprintf(nullptr, 0, format, args) + 1;
+  va_end(args);
+  
+  result.resize(size);
+  va_start(args, format);
+  vsnprintf((char*) result.data(), size, format, args);
+  va_end(args);
+  
+  return result;
+}
