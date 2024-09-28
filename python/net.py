13bfc8b1d68b8cbdc506cdae954ed92ac67482b0
mortalis13 <stangerroman@gmail.com>
2024-09-28 17:12:11 +0200

    py

---
+++ python/net.py
@@ -0,0 +1,23 @@
+
+# open url in browser
+import webbrowser
+url = 'https://docs.python.org/'
+webbrowser.open_new_tab(url)
+
+
+# raw http
+import socket
+
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('data.pr4e.org', 80))
+
+cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
+mysock.send(cmd)
+
+while True:
+    data = mysock.recv(512)
+    if len(data) < 1:
+        break
+    print(data.decode(), end='')
+
+mysock.close()
